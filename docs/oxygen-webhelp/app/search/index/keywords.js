2ed4c03f7056602d917cbb32e2feb565a75ac218
Freyaaf <65757355+Freyaaf@users.noreply.github.com>
2024-12-15 17:06:17 +0800

    add dita source and webhelp

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,15 @@
+var keywords=[{w:"Airplay\u53C2\u6570\u4FE1\u606F",p:["p0"]},{w:"\u4F7F\u7528HTMI\u7EBF\u8FDB\u884C\u6295\u5C4F",p:["p1"]},{w:"/",p:["p1"]},{w:"connecting",p:["p1"]},{w:"with",p:["p1","p3","p4","p5"]},{w:"HTMI",p:["p1","p4","p8"]},{w:"Context",p:["p2"]},{w:"Sensitive",p:["p2"]},{w:"Help",p:["p2"]},{w:"how",p:["p3","p4"]},{w:"to",p:["p3","p4","p6"]},{w:"connect",p:["p3"]},{w:"your",p:["p3","p5"]},{w:"iPad",p:["p3","p5"]},{w:"Airplay/Airplay\u6295\u5C4F\u6B65\u9AA4",p:["p3"]},{w:"mirror",p:["p4","p5"]},{w:"Airplay/\u5982\u4F55\u4F7F\u7528Airplay\u5C06iPad\u6295\u5C4F\u81F3\u6295\u5F71\u4EEA",p:["p5"]},{w:"unable",p:["p6"]},{w:"mirror/\u4E0D\u80FD\u6295\u5C4F",p:["p6"]},{w:"what",p:["p7","p8"]},{w:"is",p:["p7","p8"]},{w:"Airplay",p:["p7"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1, 2, 3, 4, 5];
+ph["p2"]=[6, 7, 8];
+ph["p3"]=[9, 10, 11, 12, 13, 4, 14];
+ph["p4"]=[9, 10, 15, 4, 5];
+ph["p5"]=[15, 12, 13, 4, 16];
+ph["p6"]=[17, 10, 18];
+ph["p7"]=[19, 20, 21];
+ph["p8"]=[19, 20, 5];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {0:3,1:4,8:4,6:3,7:3,3:-1,4:-1,5:3};
